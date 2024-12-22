9390043cc4f3aaef1c1d77783c29cd981c182f78
Al Bien Aculan <albienaculan@Als-MacBook-Pro.local>
2024-12-22 21:03:45 +0800

    added pickle reader in the code

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import pickle
 
 
 st.write("Hello, World!")
+
+model_="rnn"
+dataset_="2yr"
+forecast_horizon=4
+
+with open(f'pages/models/{model_}_{dataset_}_fh{forecast_horizon}.pickle', 'rb') as handle:
+        params = pickle.load(handle)
